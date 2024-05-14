cce55622d634347bea2c424aadd16641028acfa7
zqtay <zqtay96@gmail.com>
2024-05-15 00:26:49 +0800

    Refactor top bar button

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,26 +1,63 @@
 import Dropdown from "@/components/ui/dropdown";
 import { ExamContext } from "@/context/exam";
 import Link from "next/link";
-import { FC, PropsWithChildren, useContext } from "react";
-import { FaFileDownload } from "react-icons/fa";
+import { useRouter } from "next/router";
+import { ChangeEvent, FC, PropsWithChildren, useContext, useRef } from "react";
 import { FaEllipsisVertical } from "react-icons/fa6";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
-  const { examState, exportExamState } = useContext(ExamContext);
+  const router = useRouter();
+  const { examState, exportExamState, saveExamState } = useContext(ExamContext);
+  const importRef = useRef<HTMLInputElement>(null);
+  const isExamPage = router.pathname === "/exam";
+
+  const menuItems = [
+    ...(isExamPage) ? [{
+      label: <div onClick={() => importRef.current?.click()}>Import</div>,
+      value: "import"
+    }] : [],
+    ...(examState?.provider && examState?.examCode) ? [{
+      label: <div onClick={exportExamState}>Export</div>,
+      value: "export"
+    }] : [],
+  ];
+
   return <>
     <header className="border-b-2">
       <div className="container px-2 py-3 mx-auto flex w-full items-center">
-        <span className="w-6" />
-        <Link className="flex-1 text-xl font-semibold text-center" href="/">
+        <span className="flex-1" />
+        <Link className="flex-1 min-w-max text-xl font-semibold text-center" href="/">
           ExamTopics Scraper
         </Link>
-        {(examState?.provider && examState?.examCode) &&
-          <FaFileDownload
-            size="1.25rem"
-            className="w-6 cursor-pointer"
-            onClick={exportExamState}
+        <div className="flex-1">
+          <Dropdown
+            options={menuItems}
+            buttonClassName="ml-auto button-alt items-end border-0 p-0"
+            menuClassName="w-32 right-0 mt-4"
+            label={null}
+            icon={<FaEllipsisVertical
+              size="1.25rem"
+            />}
           />
-        }
+          <input
+            ref={importRef}
+            type="file"
+            accept=".json"
+            className="hidden"
+            onChange={async (e: ChangeEvent<HTMLInputElement>) => {
+              e.preventDefault();
+              const reader = new FileReader();
+              reader.onload = async (e) => {
+                const text = (e.target?.result);
+                if (typeof text !== "string") return;
+                const data = JSON.parse(text);
+                saveExamState(data);
+              };
+              const file = e.target.files?.[0];
+              if (file) reader.readAsText(file);
+            }}
+          />
+        </div>
       </div>
     </header>
     <main
