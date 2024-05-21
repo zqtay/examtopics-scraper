cab61ed47efa2f31750b89091b8ad776a5b046cd
zqtay <zqtay96@gmail.com>
2024-05-21 12:59:06 +0800

    Fix head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,26 @@ import type { AppProps } from "next/app";
 import Layout from "@/layout";
 import { SettingsProvider } from "@/context/settings";
 import { ExamStateProvider } from "@/context/exam";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <SettingsProvider>
-    <ExamStateProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ExamStateProvider>
-  </SettingsProvider>;
+  return <>
+    <Head>
+      <title>ExamTopics Scraper</title>
+      <meta name="description" content="An app for scraping and running ExamTopics questions" />
+      <meta name="robots" content="index, follow" />
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:title" content="ExamTopics Scraper"></meta>
+      <meta property="og:url" content="https://examtopics-scraper.vercel.app"></meta>
+      <meta property="og:description" content="An app for scraping and running ExamTopics questions" />
+    </Head>
+    <SettingsProvider>
+      <ExamStateProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ExamStateProvider>
+    </SettingsProvider>
+  </>;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,14 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>ExamTopics Scraper</title>
-        <meta name="description" content="An app for scraping and running ExamTopics questions" />
-        <meta name="robots" content="index, follow" />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <meta property="og:type" content="website"></meta>
-        <meta property="og:title" content="ExamTopics Scraper"></meta>
-        <meta property="og:url" content="https://examtopics-scraper.vercel.app"></meta>
-        <meta property="og:description" content="An app for scraping and running ExamTopics questions" />
       </Head>
       <body>
         <Main />
